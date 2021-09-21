85ffa78d061d34815c27c13f62b889ca29001c37
S8enson <sammb1234@gmail.com>
2021-09-22 10:59:10 +1200

    As a Searcher I would like to be able to view all claims associated with a specific SE practice - table now pulls data from mongoDB

--- client/src/pages/SE-Practice.js
+++ client/src/pages/SE-Practice.js
@@ -3,8 +3,27 @@ import Styles from "../components/tablestyle.js";
 import Table from "../components/evidencetable.js";
 import tablecolumns from "../components/tablecolumns.js";
 import Dropdown from "../components/Dropdown.js";
+import { useEffect, useState } from "react";
 
 const SEPractice = () => {
+const [articles, setArticles] = useState([{
+  title: '',
+  authors: "",
+  source: "",
+  pubyear: "",
+  doi: "",
+  claim: "", 
+  evidence: "",
+}])
+
+useEffect(() =>{
+  fetch("http://localhost:5000/api/v1").then(async res => {
+    if(res.ok) {
+      return res.json()
+    }
+  }).then(jsonRes => setArticles(jsonRes));
+},[])
+
     return (
       <div>
         <h2>Select SE Practice to get evidence for the claimed benefits</h2>
@@ -20,3 +39,4 @@ const SEPractice = () => {
 }
  
 export default SEPractice;  
+
