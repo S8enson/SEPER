71fb3b07f1b19e44c8767d56af2b801ce7d75712
royalty37 <patu_maori@hotmail.com>
2021-09-24 18:58:19 +1200

    As a Searcher I would like to be able to choose which information is shown - Added checkboxes corresponding to each column however they are not yet functional

--- client/src/components/evidencetable.js
+++ client/src/components/evidencetable.js
@@ -1,14 +1,15 @@
-import React, {useMemo} from "react";
-import articles from "../dummydata/articles.js";
-import { useTable, useSortBy, usePagination } from 'react-table';
+//import React, { useMemo, useEffect } from "react";
+//import articles from "../dummydata/articles.js";
+import React from "react";
+import { useTable, useSortBy, usePagination } from "react-table";
 
-const Table = ({columns, data}) => {
-const {
+const Table = ({ columns, data }) => {
+  const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
     prepareRow,
-    
+
     page, // Instead of using 'rows', we'll use page,
     // which has only the rows for the active page
 
@@ -28,89 +29,100 @@ const {
       data,
       initialState: { pageIndex: 0 },
     },
-    
+
     useSortBy,
     usePagination
-    )
+  );
 
-    // Render Data Table UI
-    return (
-      <>
-        <table {...getTableProps()}>
-          <thead>
-            {headerGroups.map(headerGroup => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map(column => (
-                  // Add the sorting props to control sorting. For this example
-                  // we can add them into the header props
-                  <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                    {column.render('Header')}
-                    {/* Add a sort direction indicator */}
-                    <span>
-                      {column.isSorted
-                        ? column.isSortedDesc
-                          ? ' 🔽'
-                          : ' 🔼'
-                        : ''}
-                    </span>
-                  </th>
-                ))}
+  // Render Data Table UI
+  return (
+    <>
+      {columns.map((column) => {
+        return (
+          <div key={column.id}>
+            <label style={{ fontFamily: "sans-serif" }}>
+              <input type="checkbox" />
+              {column.Header}
+            </label>
+          </div>
+        );
+      })}
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                // Add the sorting props to control sorting. For this example
+                // we can add them into the header props
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render("Header")}
+                  {/* Add a sort direction indicator */}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? " 🔽"
+                        : " 🔼"
+                      : ""}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
               </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {page.map((row, i) => {
-              prepareRow(row)
-              return (
-                <tr {...row.getRowProps()}>
-                  {row.cells.map(cell => {
-                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                  })}
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>  
-  
-       {/* Pagination */}
-       <div className="pagination">
-          <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-            {'<<'}
-          </button>{' '}
-          <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-            {'<'}
-          </button>{' '}
-          <button onClick={() => nextPage()} disabled={!canNextPage}>
-            {'>'}
-          </button>{' '}
-          <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-            {'>>'}
-          </button>{' '}
-          <span>
-            Page{' '}
-            <strong>
+            );
+          })}
+        </tbody>
+      </table>
+
+      {/* Pagination */}
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>{" "}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {"<"}
+        </button>{" "}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {">"}
+        </button>{" "}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
+        </button>{" "}
+        <span>
+          Page{" "}
+          <strong>
             {pageIndex + 1} of {pageOptions.length}
-          </strong>{' '}
+          </strong>{" "}
         </span>
         <span>
-          | Go to page:{' '}
+          | Go to page:{" "}
           <input
             type="number"
             defaultValue={pageIndex + 1}
-            onChange={e => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0
-              gotoPage(page)
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              gotoPage(page);
             }}
-            style={{ width: '100px' }}
+            style={{ width: "100px" }}
           />
-        </span>{' '}
+        </span>{" "}
         <select
           value={pageSize}
-          onChange={e => {
-            setPageSize(Number(e.target.value))
-          }}
-        >
-          {[3, 7, 15].map(pageSize => (
+          onChange={(e) => {
+            setPageSize(Number(e.target.value));
+          }}>
+          {[3, 7, 15].map((pageSize) => (
             <option key={pageSize} value={pageSize}>
               Show {pageSize}
             </option>
@@ -118,9 +130,7 @@ const {
         </select>
       </div>
     </>
-
-  )
+  );
 };
-  
-  export default Table;
-  
+
+export default Table;
