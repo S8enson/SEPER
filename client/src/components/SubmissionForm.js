1735ac3ab73c0f052f35bba9ef40a2d73cc3d13a
S8enson <sammb1234@gmail.com>
2021-10-01 16:10:15 +1300

    started TDD of submission functionality

--- client/src/components/SubmissionForm.js
+++ client/src/components/SubmissionForm.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 class SubmissionForm extends Component {
-  constructor() {
-    super();
+  constructor(onSubmit) {
+    super(onSubmit);
     this.state = {
       title: '',
       authors:'',
--- client/src/components/SubmissionForm.test.js
+++ client/src/components/SubmissionForm.test.js
@@ -1,29 +1,48 @@
 import React from "react";
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
-import { useEffect } from 'react';
+import { mount, shallow, configure } from "enzyme";
 import SubmissionForm from "./SubmissionForm";
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
+configure({adapter: new Adapter()});
 
+const setup = (props = {}) => {
+    const wrapper = mount(<SubmissionForm {...props} />);
+    return wrapper;
+  };
+  
+  describe("SubmissionForm", () => {
+    it("renders without errors", () => {
+      const wrapper = setup();
+      const component = wrapper.find(SubmissionForm);
+  
+      expect(component).toHaveLength(1);
+    });
+  
+    it("calls the onSubmit method", () => {
+      const handleSubmitMock = jest.fn();
+      const wrapper = setup({ onSubmit: handleSubmitMock });
+  
+      const form = wrapper.find("form");
+      form.simulate("submit", {});
+  
+      expect(handleSubmitMock).toHaveBeenCalledTimes(1);
+    });
+  
+    it("calls the onSubmit method with the expected value", () => {
+      const handleSubmitMock = jest.fn();
+      const wrapper = setup({ onSubmit: handleSubmitMock });
+  
+      const nameInput = wrapper.find("input[name='title']");
+      nameInput.simulate("change", {
+        target: { name: "title", value: "TEST" }
+      });
+  
+      const form = wrapper.find("form");
+      form.simulate("submit", {});
+  
+      expect(handleSubmitMock).toHaveBeenCalledWith({
+        name: "TEST"
+      });
+    });
+  });
 
-
-it ("should submit", function(){
-  var submit = jasmine.createSpy();
-  var form = {submit: submit}
-  submitform([somedata],form);
-  expect(submit).toHaveBeenCalled();
-})
-
-it ("should not submit if required data is missing", function(){
-    var submit = jasmine.createSpy();
-    var form = {submit: submit}
-    submitform([somedata],form);
-    expect(submit).toHaveBeenCalled();
-  })
-
-  it ("should submit", function(){
-    var submit = jasmine.createSpy();
-    var form = {submit: submit}
-    submitform([somedata],form);
-    expect(submit).toHaveBeenCalled();
-  })
