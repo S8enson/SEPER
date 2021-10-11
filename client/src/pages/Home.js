4b770cfe968bb594a9554aa8a3ed78d26a4930e3
S8enson <sammb1234@gmail.com>
2021-10-12 10:12:29 +1300

    minor bugfixes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,9 +3,7 @@ import React from "react";
 const Home = () =>  {
     return (
       <div>
-        <h2> How to use SEPER</h2>
-        <p>I am not sure what to put on the home page</p>
-        <p>A nice logo? Instructions?</p>
+        <h2> Welcome to SEPER!</h2>
       </div>
     );
   }
